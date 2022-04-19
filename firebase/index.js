6c6f2e6da13d2a2ef830e7661ce2259aa994546b
chimdi nze <nzechimdi@yahoo.com>
2022-04-19 11:20:58 +0100

    host complete

---
+++ firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHzgqcLBCppi7EoKgPzy6uMd8vf-WNrrU",
+  authDomain: "hostdemo-9781d.firebaseapp.com",
+  projectId: "hostdemo-9781d",
+  storageBucket: "hostdemo-9781d.appspot.com",
+  messagingSenderId: "354889553576",
+  appId: "1:354889553576:web:26ea4768a35dd83cd1949d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
